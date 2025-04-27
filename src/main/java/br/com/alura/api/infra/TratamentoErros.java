debe4ae0a9523363dfc945f3866042c2ea03129d
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-04-27 20:14:24 -0300

    🥅 feat: Tratamento de erro 404 - 27/04/2025

---
+++ src/main/java/br/com/alura/api/infra/TratamentoErros.java
@@ -0,0 +1,19 @@
+package br.com.alura.api.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratamentoErros {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity tratarErro404() {
+		return ResponseEntity.notFound().build();
+	}
+	
+	
+
+}
