49ab348fc0a470280ee7961bc1aef248245a10c6
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-05-02 22:50:18 -0300

    🔓 feat: Controle de acesso das requisições - 02/05/2025

--- src/main/java/br/com/alura/api/infra/security/SecurityFilter.java
+++ src/main/java/br/com/alura/api/infra/security/SecurityFilter.java
@@ -2,9 +2,13 @@ package br.com.alura.api.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.alura.api.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,25 +16,42 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
-
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		var tokenJWT = recuperarToken(request);
+		System.out.println("Ainda fora da condição tokenJWT!!!");
 		
-		System.out.println(tokenJWT);
+		if(tokenJWT != null) {
+			System.out.println("Entrou na condição tokenJWT!!!");
+			var subject = tokenService.getSubject(tokenJWT);
+			var usuario = repository.findByLogin(subject);
+			var authentication =  new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+			
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			System.out.println("Logado na requisição!!!");
+		}
 		
 		filterChain.doFilter(request, response);
-		
 	}
 	
 	protected String recuperarToken(HttpServletRequest request) {
 		var authorizationHeader = request.getHeader("Authorization");
-		if(authorizationHeader == null) {
-			throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+		if(authorizationHeader != null) {
+			return authorizationHeader.replace("Bearer ", "");
 		}
-		
-		return authorizationHeader.replace("Bearer ", "");
+		return null;
+		//else {
+			
+		//}
+		//throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");	
 	}
 
 }
--- src/main/java/br/com/alura/api/infra/security/TokenService.java
+++ src/main/java/br/com/alura/api/infra/security/TokenService.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import br.com.alura.api.domain.usuario.Usuario;
 
@@ -32,8 +35,19 @@ public class TokenService {
 		} catch (JWTCreationException exception){
 		    throw new RuntimeException("Erro ao gerar token JWT", exception);
 		}
-		
-		
+	}
+	
+	public String getSubject(String tokenJWT) {
+		try {
+			Algorithm algoritmo = Algorithm.HMAC256(secret);
+		   return JWT.require(algoritmo)
+		    	.withIssuer("API Voll.med")
+		        .build()
+		        .verify(tokenJWT)
+		        .getSubject();
+		} catch (JWTVerificationException exception){
+		    throw new RuntimeException("Token JWT inválido ou expirado!");
+		}
 	}
 
 	private Instant dataExpiracao() {
